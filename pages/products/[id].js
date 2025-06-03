3a18a43ad215d762091e0f6d04cebd5e258618a8
sinakarimiorg <karimisinaofficial@gmail.com>
2025-06-03 13:55:33 +0330

    develope ProductDetails page to show every product's information

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,9 +1,46 @@
+import ProductsDetails from '@/components/templates/Products/ProductDetails';
+import Comments from '@/components/templates/Testimonial/Comments';
 import React from 'react'
 
-function productDetails() {
+const Product = ({ product, comments }) => {
   return (
-    <div>productDetails</div>
-  )
+    <>
+      <ProductsDetails data={product} />
+      <Comments data={comments} />
+    </>
+  );
+};
+
+export async function getStaticPaths(context) {
+
+  const res = await fetch("http://localhost:4000/menu")
+  const products = await res.json()
+
+  const paths = products.map(product => ({
+    params: { id: String(product.id) }
+  }))
+
+  return {
+    paths,
+    fallback: false
+  }
 }
 
-export default productDetails
+export async function getStaticProps(context) {
+  const { params } = context
+
+  const productResponse = await fetch(`http://localhost:4000/menu/${params.id}`)
+  const productData = await productResponse.json()
+
+  const commentsResponse = await fetch(`http://localhost:4000/comments`)
+  const commentsData = await commentsResponse.json()
+
+  return {
+    props: {
+      product: productData,
+      comments: commentsData
+    }
+  }
+}
+
+export default Product
